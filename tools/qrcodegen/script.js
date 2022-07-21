78a4b76956abd52c11b4b81438931f6e2287d442
bersoncrios <bersondev@gmail.com>
2022-07-21 13:44:19 -0300

    ajuste layout qrcode e blog

--- tools/qrcodegen/script.js
+++ tools/qrcodegen/script.js
@@ -1,6 +1,6 @@
 function gerarQrCode() {
-   var input =  document.querySelector('textarea').value
-   var googleChartApi = 'https://chart.googleapis.com/chart?cht=qr&chs=500x500&chl='
+   var input =  document.querySelector('#entrada-qr').value
+   var googleChartApi = 'https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl='
    var qrcode = googleChartApi + encodeURIComponent(input)
    document.querySelector('#qrCodeImage').src = qrcode
 }
