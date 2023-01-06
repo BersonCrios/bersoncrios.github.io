b704bc201f704cfe7910d17af33818c4e2626283
bersoncrios <bersondev@gmail.com>
2023-01-06 09:58:06 -0300

    starting a refactorie

--- tools/qrcodegen/script.js
+++
@@ -1,6 +0,0 @@
-function gerarQrCode() {
-   var input =  document.querySelector('#entrada-qr').value
-   var googleChartApi = 'https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl='
-   var qrcode = googleChartApi + encodeURIComponent(input)
-   document.querySelector('#qrCodeImage').src = qrcode
-}
